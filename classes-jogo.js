2126eb793e52bb77a51c06cfb8a30c5bee751181
karinfaraujo <karinf.araujo@hotmail.com>
2025-07-18 16:02:09 -0300

    feat: adiciona projeto de classes de um jogo

---
+++ classes-jogo.js
@@ -0,0 +1,35 @@
+// Classe que representa um herói
+class Heroi {
+  constructor(nome, idade, tipo) {
+    this.nome = nome;
+    this.idade = idade;
+    this.tipo = tipo;
+  }
+
+  atacar() {
+    let ataque;
+
+    switch (this.tipo) {
+      case "mago":
+        ataque = "magia";
+        break;
+      case "guerreiro":
+        ataque = "espada";
+        break;
+      case "monge":
+        ataque = "artes marciais";
+        break;
+      case "ninja":
+        ataque = "shuriken";
+        break;
+      default:
+        ataque = "um ataque desconhecido";
+    }
+
+    console.log(`O ${this.tipo} atacou usando ${ataque}`);
+  }
+}
+
+// Criando um herói do tipo ninja
+const heroiNinja = new Heroi("Ryu", 25, "ninja");
+heroiNinja.atacar();
